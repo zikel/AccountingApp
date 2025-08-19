29fe1102bcf537ea981aaf51c4dff9bb94c5f5f8
Zike Lin <pampala@me.com>
2025-08-20 05:42:17 +0800

    v1

--- kotlin/src/main/kotlin/com/rillet/codingchallenge/accounting/application/SplitAmountsUseCase.kt
+++ kotlin/src/main/kotlin/com/rillet/codingchallenge/accounting/application/SplitAmountsUseCase.kt
@@ -1,11 +1,44 @@
 package com.rillet.codingchallenge.accounting.application
 
-import org.springframework.stereotype.Service
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.time.Month
 import javax.money.MonetaryAmount
+import org.javamoney.moneta.Money
+import org.springframework.stereotype.Service
+
+data class MonthlyAmounts(val amounts: List<MonetaryAmount>)
+
+enum class RoundingStrategy(val month: Month) {
+    FIRST(Month.JANUARY),
+    MIDDLE(Month.JUNE),
+    LAST(Month.DECEMBER)
+}
 
 @Service
 class SplitAmountsUseCase {
-    fun execute(amount: MonetaryAmount?): Any {
-        TODO("not implemented")
+    fun execute(
+            amount: MonetaryAmount,
+            roundingStrategy: RoundingStrategy = RoundingStrategy.LAST
+    ): MonthlyAmounts {
+        // TODO: Validation
+
+        val currency = amount.currency
+        val total = amount.number.numberValue(BigDecimal::class.java)
+
+        val baseMonthlyAmount = total.divide(BigDecimal(12), 2, RoundingMode.DOWN)
+        val sumOfMonthlyAmounts = baseMonthlyAmount.multiply(BigDecimal(12))
+        val roundingDifference = total.subtract(sumOfMonthlyAmounts)
+
+        val amounts = mutableListOf<MonetaryAmount>()
+
+        for (month in Month.values()) {
+            if (month == roundingStrategy.month) {
+                amounts.add(Money.of(baseMonthlyAmount.add(roundingDifference), currency))
+            } else {
+                amounts.add(Money.of(baseMonthlyAmount, currency))
+            }
+        }
+        return MonthlyAmounts(amounts)
     }
 }
--- kotlin/src/test/kotlin/com/rillet/codingchallenge/accounting/application/SplitAmountsUseCaseTest.kt
+++ kotlin/src/test/kotlin/com/rillet/codingchallenge/accounting/application/SplitAmountsUseCaseTest.kt
@@ -3,6 +3,7 @@ package com.rillet.codingchallenge.accounting.application
 import com.rillet.codingchallenge.accounting.dollars
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 import strikt.assertions.isNotEqualTo
 
 class SplitAmountsUseCaseTest {
@@ -14,5 +15,63 @@ class SplitAmountsUseCaseTest {
         val result = useCase.execute(dollars(1200))
 
         expectThat(result).isNotEqualTo(null)
+    }
+
+    @Test
+    fun `handles even division`() {
+        val result = useCase.execute(dollars(1200))
+
+        expectThat(result.amounts.size).isEqualTo(12)
+        for (i in 0 until 12) {
+            expectThat(result.amounts[i]).isEqualTo(dollars(100))
+        }
+    }
+
+    @Test
+    fun `handles rounding difference - default adjustment month`() {
+        val result = useCase.execute(dollars(1000))
+
+        expectThat(result.amounts.size).isEqualTo(12)
+        for (i in 0 until 11) {
+            expectThat(result.amounts[i]).isEqualTo(dollars(83.33))
+        }
+        expectThat(result.amounts[11]).isEqualTo(dollars(83.37))
+    }
+
+    @Test
+    fun `handles rounding difference - first month`() {
+        val result = useCase.execute(dollars(1000), RoundingStrategy.FIRST)
+
+        expectThat(result.amounts.size).isEqualTo(12)
+        expectThat(result.amounts[0]).isEqualTo(dollars(83.37))
+        for (i in 1 until 12) {
+            expectThat(result.amounts[i]).isEqualTo(dollars(83.33))
+        }
+    }
+
+    @Test
+    fun `handles rounding difference - middle month`() {
+        val result = useCase.execute(dollars(1000), RoundingStrategy.MIDDLE)
+
+        expectThat(result.amounts.size).isEqualTo(12)
+
+        for (i in 0 until 12) {
+            if (i == 5) {
+                expectThat(result.amounts[i]).isEqualTo(dollars(83.37))
+            } else {
+                expectThat(result.amounts[i]).isEqualTo(dollars(83.33))
+            }
+        }
+    }
+
+    @Test
+    fun `handles rounding difference - last month`() {
+        val result = useCase.execute(dollars(1000))
+
+        expectThat(result.amounts.size).isEqualTo(12)
+        for (i in 0 until 11) {
+            expectThat(result.amounts[i]).isEqualTo(dollars(83.33))
+        }
+        expectThat(result.amounts[11]).isEqualTo(dollars(83.37))
     }
 }
